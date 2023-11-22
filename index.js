8bcff3e802b62ce0d0f9e9a50deabcbb5c184b5a
naveenterance <naveenterance@gmail.com>
2023-11-22 18:50:35 +0530

    no more cors error

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db");
 const routes = require("./routes");
+var cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/", routes);
